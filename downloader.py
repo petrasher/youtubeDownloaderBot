cc57a83aac04e11dcf2a346144e85f2be361f977
prtrasher <patrashai@gmail.com>
2024-05-26 15:04:45 +0300

    added downloading message, added function of deleting necessary messages

--- downloader.py
+++ downloader.py
@@ -20,32 +20,33 @@ async def start_download(message: types.Message):
 
 @dp.message()
 async def process_audio_url(message: types.Message):
-    try:
-        link = message.text
+    link = message.text
 
-        audio_info = YouTube(link)
-        audio_title = re.sub(r'[/:"*?<>|.,#]', '', audio_info.title)
-        file_path = os.path.abspath(f"{audio_title}.mp3")
-        print(audio_title)
+    audio_info = YouTube(link)
+    audio_title = re.sub(r'[/:"*?<>|.,#]', '', audio_info.title)
+    file_path = os.path.abspath(f"{audio_title}.mp3")
+    download_message = await message.answer("ОЖИДАЙТЕ! идет загрузка файла...")
+    print(audio_title)
 
-        audio = audio_info.streams.filter(only_audio=True).first()
-        audio.download(filename=f"{audio_title}.mp3")
-        print('Загрузка завершена')
+    audio = audio_info.streams.filter(only_audio=True).first()
+    audio.download(filename=f"{audio_title}.mp3")
+    print('Загрузка завершена')
 
-        file_size = os.path.getsize(file_path) / (1024 * 1024)
-        if file_size > 50:
-            await message.answer("Файл слишком большой для отправки (больше 50MB).")
-            os.remove(file_path)
-            logging.info(f'Файл удален из-за большого размера: {file_path}')
-            return
-
-        file_path = os.path.abspath(f"{audio_title}.mp3")
-        audio = FSInputFile(file_path)
-        await bot.send_audio(message.chat.id, audio)
+    file_size = os.path.getsize(file_path) / (1024 * 1024)
+    if file_size > 50:
+        await message.answer("Файл слишком большой для отправки (больше 50MB).")
         os.remove(file_path)
-        print('Видео удалено')
-    except:
-        print("!!")
+        logging.info(f'Файл удален из-за большого размера: {file_path}')
+        return
+
+    file_path = os.path.abspath(f"{audio_title}.mp3")
+    audio = FSInputFile(file_path)
+    await bot.send_audio(message.chat.id, audio)
+    await bot.delete_message(message.chat.id, message.message_id)
+    await bot.delete_message(message.chat.id, download_message.message_id)
+    os.remove(file_path)
+    print('Видео удалено')
+
 
 
 async def main():
